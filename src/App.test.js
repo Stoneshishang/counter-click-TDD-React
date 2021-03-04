03fe61ccedbddbdb7486d9167a0214343139ae12
Shang Shi <stoneshishang@gmail.com>
2021-03-04 10:51:55 -0500

    more data-test attribut test and some refactor

--- src/App.test.js
+++ src/App.test.js
@@ -2,21 +2,35 @@ import App from './App';
 import Enzyme, {shallow } from 'enzyme';
 import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17'
 
-Enzyme.configure({adapter: new EnzymeAdapter()})
+Enzyme.configure({adapter: new EnzymeAdapter()});
+
+/**
+  Factory function to create a ShallowWrapper for the App component.
+  @function setup
+  @function {ShalllowWrapper}
+ */
+const setup = () => shallow(<App/>)
+
+const findByTestAttr = (wrapper, value) => wrapper.find(`[data-test='${value}']`);
 
 test("renders without error", () => {
-  const wrapper = shallow(<App/>)
-  const appComponent = wrapper.find("[data-test='component-app']") //make sure there is no space around '=' in the .find(), otherwise erroe with be thrown,
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper,'component-app')//make sure there is no space around '=' in the .find(), otherwise erroe with be thrown,
                                                                   // because equal sign won't be valid in querySelector. 
   expect(appComponent.length).toBe(1);
 })
 
 test("renders button", () => {
+const wrapper = setup();
+  const button = findByTestAttr(wrapper, 'increment-counter');
+  expect(button.length).toBe(1);
 
 })
 
 test("renders counter display", () => {
-
+  const wrapper = setup();
+  const counterDisplay = findByTestAttr(wrapper, 'counter-display');
+  expect(counterDisplay.length).toBe(1);
 })
 
 test("counter starts at 0", () => {
